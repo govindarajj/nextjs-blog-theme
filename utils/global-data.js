bb6d9a59e6a1ec60712aca582c879337f8db005b
govindarajj <106022701+govindarajj@users.noreply.github.com>
2025-08-16 19:20:24 +0530

    Test - 2

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Govind JJ';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'DevOps Engineer | Cloud Architect | Open Source Enthusiast';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
